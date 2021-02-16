c68764e218c80d2da2fc4cbc0bc17d6d6f4aac52
FLemon <jinfengxie1986@gmail.com>
2021-02-16 13:52:11 +0000

    add images slide and basic form controls

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,16 +1,45 @@
+import { useContext, useState } from 'react'
 import {
   Center, GridItem, Grid, SimpleGrid, Flex, Spacer, Box, Image, FormControl, FormLabel, Select
 } from "@chakra-ui/react"
 import S from "String"
-import { Dot, CarouselProvider, DotGroup, Slider, Slide, ButtonBack, ButtonNext, ImageWithZoom } from "pure-react-carousel"
+import { CarouselContext, Dot, CarouselProvider, DotGroup, Slider, Slide, ButtonBack, ButtonNext, ImageWithZoom } from "pure-react-carousel"
 import "pure-react-carousel/dist/react-carousel.es.css"
+
+const FormControls = ({variantType, variants}) => {
+  //TODO: use product variants image to control slide
+  const carouselContext = useContext(CarouselContext)
+  const [currentSlide, setCurrentSlide] = useState(carouselContext.state.currentSlide)
+  console.log("currentSlide")
+  console.log(currentSlide)
+
+  const selectSlide = (e) => {
+    if (e.target.id === "colour") {
+      console.log(e.target.value)
+      carouselContext.setStoreState({ currentSlide: 1})
+    }
+  }
+
+  return (
+    <FormControl key={variantType.slug} isRequired id={variantType.slug}>
+      <FormLabel>{S(variantType.slug).humanize().titleCase().s}</FormLabel>
+      <Select placeholder={`Select ${variantType.slug}`} onChange={selectSlide}>
+        {variants[variantType.slug].map(v => (
+          <option key={v}>{S(v).humanize().titleCase().s}</option>
+        ))}
+      </Select>
+    </FormControl>
+  )
+}
 
 export default function ProductDetails(props) {
   const { product, categorySlug, productVariants, variantTypes } = props
   const fallbackImage = "/product-fallback.jpeg"
   const imageSrc = product.images[0] ? product.images[0].url : fallbackImage
-
   const variants = {}
+  let sliders = []
+  let dots = []
+
   variantTypes.forEach(vt => {
     variants[vt.slug] = []
   })
@@ -23,19 +52,17 @@ export default function ProductDetails(props) {
     })
   })
 
-  let sliders = []
-  let dots = []
   product.images.forEach((image, index) => {
     sliders.push(<Slide key={index} index={index}><ImageWithZoom src={image.url}></ImageWithZoom></Slide>)
     dots.push(<Dot key={index} slide={index}><Center maxW={100}><Image src={image.url} /></Center></Dot>)
   })
 
   return (
-    <Center>
-      <SimpleGrid maxW={800} columns={{sm: 1, md: 2}} spacing="4" p={10}>
-        <Box maxW={400}>
-          <CarouselProvider naturalSlideWidth={300} naturalSlideHeight={400} infinite={true}
-            visibleSlides={1} totalSlides={3} hasMasterSpinner lockOnWindowScroll>
+    <CarouselProvider naturalSlideWidth={300} naturalSlideHeight={400} infinite={true}
+      visibleSlides={1} totalSlides={3} hasMasterSpinner lockOnWindowScroll>
+      <Center>
+        <SimpleGrid maxW={800} columns={{sm: 1, md: 2}} spacing="4" p={10}>
+          <Box maxW={400}>
             <Slider>{sliders}</Slider>
             <DotGroup>
               <Spacer />
@@ -43,21 +70,14 @@ export default function ProductDetails(props) {
                 {dots}
               </SimpleGrid>
             </DotGroup>
-          </CarouselProvider>
-        </Box>
-        <Box maxW={400}>
-          {variantTypes.map(vt => (
-            <FormControl isRequired id={vt.slug}>
-              <FormLabel>{S(vt.slug).humanize().titleCase().s}</FormLabel>
-              <Select placeholder={`Select ${vt.slug}`}>
-                {variants[vt.slug].map(v => (
-                  <option key={v}>{S(v).humanize().titleCase().s}</option>
-                ))}
-              </Select>
-            </FormControl>
-          ))}
-        </Box>
-      </SimpleGrid>
-    </Center>
+          </Box>
+          <Box maxW={400}>
+            {variantTypes.map(vt => (
+              <FormControls variantType={vt} variants={variants} />
+            ))}
+          </Box>
+        </SimpleGrid>
+      </Center>
+    </CarouselProvider>
   )
 }
