22ae2ecdc803e2d7dd81dc9fe112a6a5db82504a
FLemon <jinfengxie1986@gmail.com>
2021-04-13 02:08:09 +0100

    minor color adjust

--- components/Collections.js
+++ components/Collections.js
@@ -12,20 +12,20 @@ export default function Collections(props) {
         letterSpacing="tight" lineHeight={{ md: "shorter" }}
         fontSize={{ base: "xl", md: "4xl" }}
         textAlign={{ base: "center", md: "left" }}
-        color={useColorModeValue("pink.300", "pink.400")}
+        color={useColorModeValue("pink.400", "pink.450")}
         textShadow="2px 0 currentcolor">
         {S(cat.slug).humanize().titleCase().s}
       </Heading>
       <Box fontSize={{ md: "md" }} mb={5} letterSpacing="tight" lineHeight={{ md: "shorter" }}
         textAlign={{ base: "left" }}
-        color={useColorModeValue("pink.300", "pink.350")}>
+        color={useColorModeValue("pink.400", "pink.450")}>
         {cat.description}
       </Box>
       <Button w={{ base: "full", sm: "auto" }} size="lg" as="a" href={`/shop/${cat.slug}`}
-        _hover={ useColorModeValue("pink.500", "pink.700")}
-        bg={useColorModeValue("pink.300", "pink.350")}
+        _hover={{ bg: useColorModeValue("pink.500", "pink.600") }}
+        bg={useColorModeValue("pink.400", "pink.450")}
         color={ "white" }>
-        View Products
+        Shop Products
       </Button>
     </Box>
   )
