e4098984c20b2fee58a57b54f38ed547f5650e36
FLemon <jinfengxie1986@gmail.com>
2022-03-19 11:29:14 +0000

    fix font size

--- components/DeliveryNotice.js
+++ components/DeliveryNotice.js
@@ -10,8 +10,12 @@ export default function DeliveryNotice(props) {
     <Alert alignItems="center" justifyContent="center" textAlign="center"
       status='info' height='50px' bg='pink.400'
     >
-      <AlertTitle color="white" mr={4} fontSize={{ base: 30, md: 40}}>{title}</AlertTitle>
-      <AlertDescription pt={{base: 1, md: 4}} as='u' fontWeight='bold'>
+      <AlertTitle color="white" mr={4} fontSize={{ base: 20, md: 40}}>
+        {title}
+      </AlertTitle>
+      <AlertDescription pt={{base: 1, md: 4}} as='u'
+        fontWeight='bold' fontSize={{base:10, md: 20}}
+      >
         <Link href="/shop/pages/delivery">{des}</Link>
       </AlertDescription>
     </Alert>
